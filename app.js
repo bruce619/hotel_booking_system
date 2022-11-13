c9f21f895ebe13d2e4f09241143a43ba194c8d91
Ibecheozor Chimuanya Bruce <chimuanyaibechozor@gmail.com>
2022-11-13 16:37:58 +0000

    set up index.html, app.js, and static files

---
+++ app.js
@@ -0,0 +1,22 @@
+// Imports
+const express = require('express')
+const app = express()
+const port = 3000
+
+// use static files: css, js, img
+app.use(express.static('src/public/assets'))
+// css
+app.use('/css', express.static(__dirname + 'src/public/assets/css'))
+// js
+app.use('/js', express.static(__dirname + 'src/public/assets/js'))
+// img
+app.use('/img', express.static(__dirname + 'src/public/assets/img'))
+
+
+// use html templates
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/templates/index.html')
+})
+
+// Listen to port 3000
+app.listen(port, () => console.info(`hotel booking app listening on port ${port}`))
