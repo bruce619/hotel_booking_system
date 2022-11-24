d5db6dde56b8b477ca04ef93a08b52bbde10acc8
Ibecheozor Chimuanya Bruce <chimuanyaibechozor@gmail.com>
2022-11-24 01:18:43 +0000

    set up payment form html, payment confirmation and confirmed html and added css styling

--- app.js
+++ app.js
@@ -4,18 +4,30 @@ const app = express()
 const port = 3000
 
 // use static files: css, js, img
-app.use(express.static('src/public/assets'))
+app.use(express.static('/src/public/assets'))
 // css
-app.use('/css', express.static(__dirname + 'src/public/assets/css'))
+app.use('/css', express.static(__dirname + '/src/public/assets/css'))
 // js
-app.use('/js', express.static(__dirname + 'src/public/assets/js'))
+app.use('/js', express.static(__dirname + '/src/public/assets/js'))
 // img
-app.use('/img', express.static(__dirname + 'src/public/assets/img'))
+app.use('/img', express.static(__dirname + '/src/public/assets/img'))
 
 
 // use html templates
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/templates/index.html')
+})
+
+app.get('/payment/form', (req, res) => {
+    res.sendFile(__dirname + '/src/templates/payment_form.html')
+})
+
+app.get('/payment/confirmation', (req, res) => {
+    res.sendFile(__dirname + '/src/templates/payment_confirmation.html')
+})
+
+app.get('/payment/confirmed', (req, res) => {
+    res.sendFile(__dirname + '/src/templates/payment_confirmed.html')
 })
 
 // Listen to port 3000
